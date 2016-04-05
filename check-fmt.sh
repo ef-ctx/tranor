084d48ed2b52d0e61aa8730d924a7718b45dc9b8
Francisco Souza <francisco.souza@ef.com>
2016-04-04 23:24:16 -0300

    travis: add file with instructions for testing the project

---
+++ check-fmt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+# Copyright 2016 EF CTX. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+pkgs=$(go list ./... | grep -v vendor | gsed 's;github.com/ef-ctx/tsuru-flow/;./;')
+
+files=$(gofmt -s -l $pkgs)
+if [ -n "${files}" ]; then
+	echo "gofmt -s -w required for:"
+	echo $(echo ${files} | sed 's/^/- /')
+fi
+
+go get golang.org/x/tools/cmd/vet
+go vet $pkgs
+
+go get github.com/golang/lint/golint
+golint $pkgs
+
+go get github.com/remyoudompheng/go-misc/deadcode
+deadcode $pkgs
